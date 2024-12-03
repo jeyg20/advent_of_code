02b8308193d014b922b43ad9b42a37b373178e67
Jeiso Guerrero <jei_son1516@hotmail.com>
2024-12-03 16:41:25 -0500

    solution day two part one

---
+++ day_02/solution.py
@@ -0,0 +1,24 @@
+def calculate_total_safe_reports(levels: list[int]) -> bool:
+    for i in range(len(levels) - 1):
+        if not (1 <= abs(levels[i + 1] - levels[i]) <= 3):
+            return False
+
+    increasing, decreasing = True, True
+    for i in range(1, len(levels)):
+        if levels[i] > levels[i - 1]:
+            decreasing = False
+        elif levels[i] < levels[i - 1]:
+            increasing = False
+
+    return increasing or decreasing
+
+
+if __name__ == "__main__":
+    with open("input.txt", "r") as input_file:
+        total_safe_reports = 0
+        for line in input_file:
+            levels = [int(num) for num in line.split()]
+            if calculate_total_safe_reports(levels):
+                total_safe_reports += 1
+
+        print(total_safe_reports)
